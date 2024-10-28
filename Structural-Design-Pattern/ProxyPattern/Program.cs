4c5792cf018aaa3ef6db200514efd433a0b8c0de
limon-7 <liibd7@gmail.com>
2024-10-28 15:11:09 +0600

    Feat: add proxy design pattern

---
+++ Structural-Design-Pattern/ProxyPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IVideo video = new VideoProxy("sample.mp4");
+
+            Console.WriteLine("Video object created.");
+            Console.WriteLine("Now playing video:");
+
+            video.Play();
+        }
+    }
+}
--- Structural-Design-Pattern/ProxyPattern/VideoProxy.cs
+++ Structural-Design-Pattern/ProxyPattern/VideoProxy.cs
@@ -1,7 +1,47 @@
-﻿namespace ProxyPattern
+﻿using System;
+
+namespace ProxyPattern
 {
-    public class VideoProxy
+    public class VideoProxy : IVideo
     {
-        
+        private readonly string _videoFile;
+        private RealVideo _realVideo;
+
+        public VideoProxy(string videoFile)
+        {
+            _videoFile = videoFile;
+        }
+
+        public void Play()
+        {
+            if (_realVideo == null) _realVideo = new RealVideo(_videoFile);
+            _realVideo.Play();
+        }
+    }
+
+    public interface IVideo
+    {
+        void Play();
+    }
+
+    public class RealVideo : IVideo
+    {
+        private readonly string _videoFile;
+
+        public RealVideo(string videoFile)
+        {
+            _videoFile = videoFile;
+            LoadVideo();
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"Playing video: {_videoFile}");
+        }
+
+        private void LoadVideo()
+        {
+            Console.WriteLine($"Loading video: {_videoFile}");
+        }
     }
 }
